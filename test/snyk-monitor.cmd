14d24c4c51d1d857f16ab1094b07053bf2dfdfab
Jason M Penniman <jason.penniman@milestonetg.com>
2018-05-06 20:08:57 -0400

    Snyk scripts and doc updates

---
+++ test/snyk-monitor.cmd
@@ -0,0 +1,5 @@
+call snyk monitor ../src/MilestoneTG.TransientFaultHandling
+call snyk monitor ../src/MilestoneTG.TransientFaultHandling.Http
+call snyk monitor ../src/MilestoneTG.TransientFaultHandling.Data
+call snyk monitor ../src/MilestoneTG.TransientFaultHandling.Data.SqlServer
+call snyk monitor ../src/MilestoneTG.NHibernate.TransientFaultHandling.SqlServer
---
+++ test/snyk-test.cmd
@@ -0,0 +1,5 @@
+call snyk test ../src/MilestoneTG.TransientFaultHandling
+call snyk test ../src/MilestoneTG.TransientFaultHandling.Http
+call snyk test ../src/MilestoneTG.TransientFaultHandling.Data
+call snyk test ../src/MilestoneTG.TransientFaultHandling.Data.SqlServer
+call snyk test ../src/MilestoneTG.NHibernate.TransientFaultHandling.SqlServer
